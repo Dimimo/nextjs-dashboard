f0ba04130947df6ad9845366401d8d307bfb1b1a
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-27 09:51:29 +0800

    Add Roboto font styling to welcome message in the main page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import {ArrowRightIcon} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import {roboto} from "@/app/ui/fonts";
 
 export default function Page() {
     return (
@@ -13,7 +14,7 @@ export default function Page() {
                     <div
                         className={styles.shape}
                     />
-                    <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+                    <p className={`${roboto.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
                         <strong>Welcome to Acme.</strong> This is the example for the{' '}
                         <a href="https://nextjs.org/learn/" className="text-blue-500">
                             Next.js Learn Course
