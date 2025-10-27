1981b3e6f68c8af6c05ebf173db977fa85d4b17c
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-27 12:10:52 +0800

    Add Lusitana font import and export to fonts module

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import { Roboto} from "next/font/google";
+import { Lusitana} from "next/font/google";
 
 export const inter = Inter({ weight: ['400', '700'], subsets: ['latin']});
 export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin', 'math']});
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin']})
