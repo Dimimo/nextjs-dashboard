cfbd0b6af85669c2b30949e2061e7eeda316281f
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-27 09:51:21 +0800

    Add Roboto font import and update Inter font configuration in fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
 import { Inter } from 'next/font/google';
+import { Roboto} from "next/font/google";
 
-export const inter = Inter({ subsets: ['latin']});
+export const inter = Inter({ weight: ['400', '700'], subsets: ['latin']});
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin', 'math']});
