43c832e8373ef52f9b519271f244ddf1561b9464
Dimitri Mostrey <dmostrey@yahoo.com>
2025-10-28 18:32:40 +0800

    Update skeleton component names in dashboard overview page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import {lusitana} from '@/app/ui/fonts';
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import {Suspense} from "react";
-import {LatestInvoicesSkeleton, RevenueChartSkeleton, CardSkeleton} from "@/app/ui/skeletons";
+import {LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton} from "@/app/ui/skeletons";
 
 export default async function Page() {
     return (
@@ -12,7 +12,7 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardSkeleton />}>
+                <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
             </div>
