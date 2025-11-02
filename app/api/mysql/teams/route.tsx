2c3ede70c827f1321b124d5a657fd9974112c123
Dimitri Mostrey <dmostrey@yahoo.com>
2025-11-02 17:46:48 +0800

    Update team fetching to use bigint for season and define teams as TeamTable array

--- app/api/mysql/teams/route.tsx
+++ app/api/mysql/teams/route.tsx
@@ -1,9 +1,10 @@
 import {NextRequest, NextResponse} from 'next/server'
 import {fetchTeams} from "@/app/lib/billiard/data";
+import {TeamTable} from "@/app/lib/billiard/definitions";
 
 export async function GET(request: NextRequest) {
-    let season: string = request.nextUrl.searchParams.get('season')!;
-    const teams = await fetchTeams(season);
+    let season: bigint = request.nextUrl.searchParams.get('season')! as unknown as bigint;
+    const teams: TeamTable[] = await fetchTeams(season);
 
     return NextResponse.json({teams});
 }
