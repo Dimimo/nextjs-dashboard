cd1776d2487f40c4520eac568165d5ca79fdfabb
Dimitri Mostrey <dmostrey@yahoo.com>
2025-11-07 00:01:11 +0800

    Update API functions to return specific types and improve data handling for seasons and teams

--- app/lib/billiard/apis.ts
+++ app/lib/billiard/apis.ts
@@ -1,27 +1,25 @@
-import {TeamTable} from "@/app/lib/billiard/definitions";
+import {SeasonTable, TeamTable} from "@/app/lib/billiard/definitions";
 
-
-export async function latestSeasonApi(): Promise<any> {
-    let req, data;
-    req = await fetch('http://localhost:3000/api/mysql/latest-season',
+export async function latestSeasonApi(): Promise<SeasonTable> {
+    const req = await fetch('http://localhost:3000/api/mysql/latest-season',
         {
             method: 'GET',
             cache: 'no-store'
         });
 
-    data = await req.json();
+    const data = await req.json();
     return data.season.shift();
 }
 
-export async function teamsByLatestSeasonApi(): Promise<any> {
-    const season: TeamTable = await latestSeasonApi();
-    const req = await fetch('http://localhost:3000/api/mysql/teams/?season=' + season.id,
+export async function teamsByLatestSeasonApi(): Promise<TeamTable[]> {
+    const season: SeasonTable = await latestSeasonApi();
+    const req: Response = await fetch('http://localhost:3000/api/mysql/teams/?season=' + season.id,
         {
             method: 'GET',
             cache: 'no-store'
         });
-    let data = await req.json();
-    data = data.teams;
+    let data: any = await req.json();
+    const teams: TeamTable[] = data.teams;
 
-    return data.filter((team: TeamTable) => team.name !== 'BYE');
+    return teams.filter((team: TeamTable) => team.name !== 'BYE');
 }
