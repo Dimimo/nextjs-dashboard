ae15e61970c5dc098ff423986ef0ea017b336cb5
Dimitri Mostrey <dmostrey@yahoo.com>
2025-11-06 23:49:54 +0800

    Update fetchTeams and fetchLatestSeason functions to improve type safety and error handling

--- app/lib/billiard/data.ts
+++ app/lib/billiard/data.ts
@@ -4,7 +4,7 @@ import {SeasonTable, TeamTable} from "@/app/lib/billiard/definitions";
 
 const connectionParams: IDBSettings = GetDBSettings();
 
-export async function fetchTeams(season: bigint) {
+export async function fetchTeams(season: bigint): Promise<TeamTable[]> {
     try {
         const connection = await mysql.createConnection(connectionParams);
         const sql = `SELECT teams.*, venues.name AS venue_name
@@ -12,27 +12,26 @@ export async function fetchTeams(season: bigint) {
                               JOIN venues ON venues.id = teams.venue_id
                      WHERE teams.season_id = ?
                      ORDER BY teams.name`;
-        let values: any[] = [season];
+        let values: [SeasonTable['id']] = [season];
         const [rows] = await connection.execute(sql, values) as [TeamTable[], any];
         connection.end();
 
         return rows;
-    } catch (error) {
+    } catch (error: unknown) {
         console.error('Database Error:', error);
         throw new Error('Failed to fetch the team.');
     }
 }
 
-export async function fetchLatestSeason() {
+export async function fetchLatestSeason(): Promise<SeasonTable> {
     try {
         const connection = await mysql.createConnection(connectionParams);
         const sql = 'SELECT seasons.* FROM seasons ORDER BY id DESC LIMIT 1';
-        let values: any[] = [];
-        const [rows] = await connection.execute(sql, values) as [SeasonTable[], any];
+        const [seasons] = await connection.execute(sql) as [SeasonTable[], any];
         connection.end();
 
-        return rows[0];
-    } catch (error) {
+        return seasons[0];
+    } catch (error: unknown) {
         console.error('Database Error:', error);
         throw new Error('Failed to fetch the latest season.');
     }
